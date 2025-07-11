2afacf743be85fa520b4085aac2aaa878cd1bf7b
hemanth <hemanth02135@gmail.com>
2025-07-11 23:45:02 +0530

    AuthController

---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,51 @@
+import { db } from "../config/db";
+
+interface User {
+  username: string;
+  email?: string;
+  avatarUrl?: string;
+  githubId?: string;
+}
+
+export const AuthController = async ({
+  username,
+  email,
+  avatarUrl,
+  githubId,
+}: User) => {
+  try {
+    const existingUser = await db.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (existingUser) {
+      throw new Error("User already exists");
+    }
+
+    const newUser = await db.user.create({
+      data: {
+        username,
+        email: email ?? null,
+        avatarUrl: avatarUrl ?? null,
+        githubId: githubId ?? null,
+      },
+    });
+
+    return newUser;
+  } catch (error) {
+    console.error("Error in Login:", error);
+    throw error;
+  }
+};
+
+
+// (async () => {
+//   await AuthController({
+//     username: "jerusha",
+//     email: "jerusha@gmail.com",
+//     avatarUrl: "https://github.com/jerusha.png",
+//     githubId: "jerusha_1431",
+//   });
+// })();
