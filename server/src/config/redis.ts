dea2d381fa834954ae6cf295f519bcf430625495
hemanth <hemanth02135@gmail.com>
2025-07-24 14:50:51 +0530

    redis_cache

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,8 +1,6 @@
 import Redis from "ioredis";
 
-export const redis = new Redis(
-  "rediss://default:AVNS_b1EDkNmX4YhqUylEC5Y@valkey-1ff3fbe2-hemanth02135-c08c.d.aivencloud.com:26970"
-);
+export const redis = new Redis(process.env.REDIS_URL!);
 
 redis.on("connect", () => {
   console.log("redis connected");
